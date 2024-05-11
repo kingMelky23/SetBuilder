4c296ab968f31cda75993716e8c390467204c5b2
Melchizedek Tetteh <Tettehmel@gmail.com>
2024-05-11 19:01:56 -0400

    MT - create setBuilder - backend

---
+++ src/main/groovy/com/tetteh/SetBuilder/SetBuilderApplication.groovy
@@ -0,0 +1,13 @@
+package com.tetteh.SetBuilder
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SetBuilderApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SetBuilderApplication, args)
+	}
+
+}
